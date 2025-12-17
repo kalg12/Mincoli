a9791d91d2c2e31a28be3c9d8ddb69f90a185ba8
Kevin Luciano <luciano19940@hotmail.com>
2025-12-16 23:01:23 -0600

    feat: Añadir soporte para múltiples idiomas en la plantilla de página

--- resources/views/components/layouts/page.blade.php
+++ resources/views/components/layouts/page.blade.php
@@ -2,6 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         @include('partials.head')
+        <!-- Font Awesome for icons -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <title>{{ $title ?? 'Mincoli' }}</title>
     </head>
     <body class="min-h-screen bg-white antialiased">
